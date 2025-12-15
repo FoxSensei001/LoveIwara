5ae06e2ea376faac5da13dcf1a18a51432f53ca7
miximixihuabulaji <miximixihuabulaji@proton.me>
2025-12-15 23:31:34 +0800

    feat: '发布 0.4.1'

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -8,7 +8,7 @@ class CommonConstants {
   CommonConstants._internal();
 
   // 应用版本
-  static const String VERSION = '0.4.0';
+  static const String VERSION = '0.4.1';
 
   // 应用名称
   static String? applicationName = 'i_iwara';
